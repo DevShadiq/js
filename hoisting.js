09b9f0115e383dfe423c5953b7b1121d024490ec
DevShadiq <shadiq1297@gmail.com>
2022-10-25 22:56:17 +0600

    hoisting

---
+++ hoisting.js
@@ -0,0 +1,12 @@
+/** @format */
+
+function myFunc() {
+  language = 'JavaScript';
+  console.log(language);
+  var language;
+}
+myFunc();
+
+console.log(language);
+var language;
+language = 'Java';
